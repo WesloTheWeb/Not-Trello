651924aa58331b10265b7a08edd0f9cb17f9d47f
Wesley <wesleyadwebster@gmail.com>
2021-10-23 18:24:30 -0500

    Styling changes to inputs

--- src/containers/createTicketModal/createTicketModal.js
+++ src/containers/createTicketModal/createTicketModal.js
@@ -3,8 +3,7 @@ import { ModalContext } from '../../Contexts/ModalContext';
 import { useForm } from "react-hook-form";
 import classes from './CreateTicketModal.module.css';
 
-const { modalContainer, closeButton } = classes;
-
+const { modalContainer, modalCTAContainer, closeButton, fieldLabel } = classes;
 
 const CreateTicketModal = (props) => {
 
@@ -24,13 +23,17 @@ const CreateTicketModal = (props) => {
         <form className={modalContainer} onSubmit={handleSubmit(onSubmit)}>
             <h2>Create a new ticket</h2>
             {/* register your input into the hook by invoking the "register" function */}
-            <input defaultValue="test" {...register("example")} />
-
-            {/* include validation with required or other standard HTML validation rules */}
-            <input {...register("exampleRequired", { required: true })} />
+            <div className={fieldLabel}>
+                <label>Title</label>
+                <input placeholder="Title" {...register("title")} />
+            </div>
+            <div className={fieldLabel}>
+                <label>Description</label>
+                <textarea type="text" size="50" placeholder="Description" {...register("description", { required: true })} />
+            </div>
             {/* errors will return when field validation fails  */}
             {errors.exampleRequired && <span>This field is required</span>}
-            <div>
+            <div className={modalCTAContainer}>
                 <button type="submit">Submit</button>
                 <button className={closeButton} onClick={handleClose}>Close</button>
             </div>
